651491b0e68bc02b98c9d5fc99df454290647554
Defeaded-S <72038136+Defeaded-S@users.noreply.github.com>
2025-05-17 23:59:11 +0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 REPO_DIR="."
 REPO_NAME="classic"
-for i in x86_64-i586 i686 x86_64 noarch; do
+for i in x86_64-i586 x86_64 noarch; do
   mkdir -p "$REPO_DIR/$i/base" "$REPO_DIR/$i/RPMS.$REPO_NAME"
 done
