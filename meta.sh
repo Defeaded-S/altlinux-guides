4234282d4c9e0c7e882382cae983a70fcb20a090
Defeaded-S <72038136+Defeaded-S@users.noreply.github.com>
2025-05-18 00:33:09 +0300

    Update meta.sh

--- meta.sh
+++ meta.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+REPO_DIR="$1"
 REPO_NAME="$2"
 
 for arch in x86_64-i586 x86_64 noarch; do
-  genbasedir --bloat --progress --topdir=. $arch $REPO_NAME
+  genbasedir --bloat --progress --topdir=$REPO_DIR $arch $REPO_NAME
 done
