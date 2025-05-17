4026d38dcbe603b10628b49a455b2243a50b783f
Defeaded-S <72038136+Defeaded-S@users.noreply.github.com>
2025-05-17 18:30:38 +0300

    Update easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+cd ~/
+apt-get install -y git
+git clone https://github.com/Defeaded-S/altlinux-guides.git
+mkdir -p /var/www/html/altlinux
+mv ~/altlinux-guides/create.sh /var/www/html/altlinux/
+cd /var/www/html/altlinux
+./create.sh
+apt-get install -y nginx
+systemctl enable -now nginx
+mv ~/altlinux-guides/altlinux.conf /etc/nginx/sites-available.d/
+ln -s /etc/nginx/sites-available.d/altlinux.conf /etc/nginx/sites-enabled.d/altlinux.conf
+systemctl restart nginx
+apt-get install /usr/bin/packages
+cd ~/altlinux-guides
