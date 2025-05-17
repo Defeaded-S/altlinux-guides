a475bd5f7f19a307c5183190d668709825756f6c
Defeaded-S <72038136+Defeaded-S@users.noreply.github.com>
2025-05-17 23:51:16 +0300

    Update easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -7,11 +7,10 @@ wait
 mkdir -p /var/www/html/altlinux
 cd /var/www/html/altlinux
 cat ~/altlinux-guides/create.sh | bash
-wait
-systemctl enable -now nginx
+cat ~/altlinux-guides/meta.sh | bash
 wait
 cp ~/altlinux-guides/altlinux.conf /etc/nginx/sites-available.d/
 ln -s /etc/nginx/sites-available.d/altlinux.conf /etc/nginx/sites-enabled.d/altlinux.conf
-systemctl restart nginx
+systemctl enable -now nginx
 wait
 apt-get install -y /usr/bin/packages
